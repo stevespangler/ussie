420b175b8b5f4b2101831a39f8188d74716f1687
stevespangler <steve_spangler@mckinsey.com>
2022-07-25 11:48:21 -0400

    adding function and package documentation

--- R/matches.R
+++ R/matches.R
@@ -1,3 +1,16 @@
+#' Create a standardized tibble for league data
+#'
+#' Given a league-play data frame from {engsoccer}, returns a tibble with
+#' standardised column-names and types, e.g. `date` is a `Date`.
+#'
+#' @param data_engsoc dataframe from engsoccer package
+#' @param country_name league country name, one of Spain, Italy, England,
+#' or Holland
+#'
+#' @return dataframme, tibble
+#' @export
+#'
+#' @examples uss_make_matches(engsoccerdata::spain, "Spain")
 uss_make_matches <- function(data_engsoc, country_name) {
   result <-
     data_engsoc |>
